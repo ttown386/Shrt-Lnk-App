276d9bf1cb022da7ab55bc5079b95a16668527e2
Tyler Townsend <ttown@knights.ucf.edu>
2018-12-13 22:28:31 -0500

    Links may be copied via clipboard and state is set for copied links for 1 sec

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,19 +1,45 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      justCopied: false
+    };
+  }
+
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refs.copy);
+    this.justCopied = false;
+
+    this.clipboard.on('success', () => {
+      this.setState({ justCopied: true });
+      setTimeout(() => this.setState({ justCopied: false}), 1000);
+    }).on('error', () => {
+      alert('Unable to copy. Please manually copy the link');
+    });
+  }
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+        {this.state.justCopied ? 'Copied' : 'Copy'}
+        </button>
       </div>
     );
   }
 }
 
-LinksListItems.propTypes = {
-  _id : React.PropTypes.string.isRequired,
-  url : React.PropTypes.string.isRequired,
-  userId : React.PropTypes.string.isRequired,
-  shortUrl : React.PropTypes.string.isRequired
+LinksListItem.propTypes = {
+  _id : PropTypes.string.isRequired,
+  url : PropTypes.string.isRequired,
+  userId : PropTypes.string.isRequired,
+  shortUrl : PropTypes.string.isRequired
 }
