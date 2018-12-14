33967c9b685f34b204007bf4d2d295a704141ef6
Tyler Townsend <ttown@knights.ucf.edu>
2018-12-14 10:51:08 -0500

    showVisible state defines a controlled input

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 
 export default class AddLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    };
+  }
 
   onSubmit(e) {
     e.preventDefault();
 
-    const url = this.refs.url.value.trim();
+    const { url } = this.state
     if (url) {
-      Meteor.call('links.insert', url);
-      this.refs.url.value = '';
+      Meteor.call('links.insert', url, (err) => {
+        if (!err) {
+          this.setState({ url: ''});
+        }
+      });
     }
+  }
+
+  onChange(e) {
+    this.setState({
+      url: e.target.value
+    });
   }
 
   render() {
@@ -18,7 +33,11 @@ export default class AddLink extends React.Component {
       <div>
         <p>Add Link</p>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL"/>
+          <input 
+            type="text" 
+            placeholder="URL"
+            value={this.state.url}
+            onChange={this.onChange.bind(this)}/>
           <button>Add Link</button>
         </form>
       </div>
--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import { Session } from 'meteor/session';
+import { Tracker } from 'meteor/tracker';
 
-export default () => {
-  return (
-    <div>
-      <label>
-        <input type="checkbox" onChange={(e) => {
-          Session.set('showVisible', !e.target.checked);
-        }}/>
-        Show Hidden Links
-      </label>
-    </div>
-  );
+export default class LinksListFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showVisible: true
+    };
+  }
+
+  componentDidMount() {
+    this.tracker = Tracker.autorun(() => {
+      this.setState({
+        showVisible: Session.get('showVisible')
+      })
+    });
+  }
+  componentWillUnmount() {
+    this.tracker.stop();
+  }
+  render() {
+    return (
+      <div>
+        <label>
+          <input type="checkbox" checked={!this.state.showVisible} onChange={(e) => {
+            console.log('here');
+            Session.set('showVisible', !e.target.checked);
+          }}/>
+          Show Hidden Links
+        </label>
+      </div>
+    );
+  }
 }
